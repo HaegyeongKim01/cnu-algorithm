ae54e283e8f64334549a9088483da7e49be453b8
HaegyeongKim <Haegyeon.kim01@gmail.com>
2022-12-22 07:46:21 +0900

    upload

---
+++ AL01/AL01_202004182_김해경/src/graph/AppView.java
@@ -0,0 +1,15 @@
+package graph;
+
+
+public final class AppView {
+	
+	private AppView() {	
+	}
+	
+	public static void outputLine(String aString) {
+		System.out.println(aString);
+	}
+	public static void output(String aString) {
+		System.out.print(aString);
+	}
+}
---
+++ AL01/AL01_202004182_김해경/src/graph/_Main_AL01_202004182_김해경.java
@@ -0,0 +1,12 @@
+package graph;
+
+public class _Main_AL01_202004182_김해경 {
+
+	public static void main(String[] args) {
+		
+		AppController appController = new AppController();
+		appController.run();
+	
+	}
+
+}
